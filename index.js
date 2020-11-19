e6c980393913f6d17d1756e37c76d61f71ea4723
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-19 14:56:06 -0500

    Add nyt api access

---
+++ index.js
@@ -0,0 +1,41 @@
+//set up
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const ejsLayouts = require('express-ejs-layouts');
+const port = process.env.PORT || 3000;
+const api_key = process.env.API_KEY;
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.get('/:title', (req, res) => {
+    axios.get(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${req.params.title}&api-key=${api_key}`).then(response => {
+        if (response.status === 200) {
+            let movies = [];
+            for (let i = 0; i < response.data.results.length; i++) {
+                let movieResult = response.data.results[i];
+                let movieFinal = {
+                    title: movieResult.display_title,
+                    byline: movieResult.byline,
+                    headline: movieResult.headline,
+                    pubDate: movieResult.publication_date,
+                    url: movieResult.link.url
+                }
+                movies.push(movieFinal);
+            }
+            return movies;
+        }
+    }).then(movies => {
+        console.log(movies);
+        res.render('results', { movies })
+    }).catch(err => console.log(err))
+
+
+})
+
+app.listen(port, () => console.log('start'));
