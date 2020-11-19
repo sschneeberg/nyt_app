9b04b8784d8650ca16a2b7607c2f01d815f3774c
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-19 17:04:32 -0500

    Render home route

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: false }));
 
 //db test
-
+/*
 db.movie.findOrCreate({
     where: {
         title: 'Test movie',
@@ -25,31 +25,40 @@ db.movie.findOrCreate({
 
     }
 }).then(response => console.log(response));
+*/
 
 //routes
 app.get('/:title', (req, res) => {
     axios.get(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${req.params.title}&api-key=${api_key}`).then(response => {
         if (response.status === 200) {
-            let movies = [];
             for (let i = 0; i < response.data.results.length; i++) {
                 let movieResult = response.data.results[i];
-                let movieFinal = {
+                /*let movieFinal = {
                     title: movieResult.display_title,
                     byline: movieResult.byline,
                     headline: movieResult.headline,
                     pubDate: movieResult.publication_date,
                     url: movieResult.link.url
-                }
-                movies.push(movieFinal);
+                } */
+                db.movie.findOrCreate({
+                    where: { title: movieResult.display_title },
+                    defaults: {
+                        byline: movieResult.byline,
+                        headline: movieResult.headline,
+                        pub_date: movieResult.publication_date,
+                        url: movieResult.link.url
+                    }
+                }).then(([movie, created]) => {
+                    console.log(created);
+                }).then(() => {
+                    db.movie.findAll().then((movies) => {
+                        console.log(movies)
+                        res.render('results', { movies })
+                    })
+                })
             }
-            return movies;
         }
-    }).then(movies => {
-        console.log(movies);
-        res.render('results', { movies })
-    }).catch(err => console.log(err))
-
-
+    })
 })
 
 app.listen(port, () => console.log('start'));
